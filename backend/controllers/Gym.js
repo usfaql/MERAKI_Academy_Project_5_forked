f1c1a76e7c7441741db8d1f0fbe1674853f92b81
usfaql <usfaql@gmail.com>
2024-02-09 19:37:51 +0300

    get All User In Gym

--- backend/controllers/Gym.js
+++ backend/controllers/Gym.js
@@ -54,6 +54,23 @@ const addNewUserInGym = (req,res)=>{
     });
 }
 
+const getAllUserInGym = (req,res)=>{
+    const gym_id = 2;
+    const provider = [gym_id]
+    pool.query(`SELECT * FROM gym_user INNER JOIN users ON gym_user.user_id = users.id WHERE gym_id = $1`, provider).then((result) => {
+        res.status(200).json({
+            success : true,
+            message : `All User In Gym`,
+            result : result.rows
+        });
+    }).catch((err) => {
+        res.status(500).json({
+            success : false,
+            message : `Error server`,
+            err
+        });
+    });
+}
 const addNewCoachInGym = (req,res) =>{
     const {gym_id, coach_id} = req.body;
     const provider = [gym_id, coach_id];
@@ -86,5 +103,6 @@ module.exports = {
     createGym,
     addNewCoachInGym,
     addNewUserInGym,
-    getAllGym
+    getAllGym,
+    getAllUserInGym
 }
--- backend/routes/Gym.js
+++ backend/routes/Gym.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-const {createGym, addNewUserInGym,addNewCoachInGym,getAllGym} = require("../controllers/Gym");
+const {createGym, addNewUserInGym,addNewCoachInGym,getAllGym, getAllUserInGym} = require("../controllers/Gym");
 
 const gymsRouter = express.Router();
 
 gymsRouter.post("/", createGym);
 gymsRouter.get("/", getAllGym);
 gymsRouter.post("/gym_user", addNewUserInGym)
+gymsRouter.get("/gym_user", getAllUserInGym)
 gymsRouter.post("/gym_coach", addNewCoachInGym)
 
 module.exports = gymsRouter
