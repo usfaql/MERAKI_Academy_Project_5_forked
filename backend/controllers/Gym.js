3a6ae685f759ac228fbe5f1d0090e0926a1100fc
unknown <usfaql@gmail.com>
2024-02-10 00:23:30 +0300

    Upgrade Function add user in Gym

--- backend/controllers/Gym.js
+++ backend/controllers/Gym.js
@@ -39,19 +39,41 @@ const getAllGym = (req, res)=>{
 const addNewUserInGym = (req,res)=>{
     const {gym_id, user_id} = req.body;
     const provider = [user_id, gym_id];
-    pool.query(`INSERT INTO gym_user(user_id, gym_id) VALUES ($1,$2)`, provider).then((result) => {
-        res.status(201).json({
-            success : true,
-            message : "User Add Successfully In Gym",
-            result : result.rows
-        })
-    }).catch((err) => {
-        res.status(500).json({
-            success : false,
-            message : `Server error`,
-            error : err.message
-        })
-    });
+    pool.query(`SELECT user_id FROM gym_user WHERE user_id = $1`,[user_id]).then((result)=>{
+        if(result.rows.length !== 0 ){
+            res.status(201).json({
+                success : true,
+                message : `The User Already Exist in Gym`
+            });
+            
+        }else{
+            pool.query(`SELECT coach_id FROM gym_coach WHERE coach_id = $1`, [user_id]).then((result)=>{
+                if(result.rows.length !== 0){
+                    res.status(201).json({
+                        success : false,
+                        message : `The User Already Exist Coach In Gym`
+                    })
+                }else{
+                    pool.query(`INSERT INTO gym_user(user_id, gym_id) VALUES ($1,$2) RETURNING *`, provider).then((result) => {
+                        res.status(201).json({
+                            success : true,
+                            message : "User Add Successfully In Gym",
+                            result : result.rows
+                        })
+                    }).catch((err) => {
+                        res.status(500).json({
+                            success : false,
+                            message : `Server error`,
+                            error : err.message
+                        });
+                    });
+                }
+            })
+
+        }
+        
+    })
+
 }
 
 const getAllUserInGym = (req,res)=>{
@@ -78,7 +100,7 @@ const deleteUserInGym = async(req,res)=>{
     await pool.query(`DELETE FROM gym_user WHERE user_id = $2 AND gym_id = $1`, provider).then(async(result) => {
         res.status(201).json({
             success : true,
-            message : `Coach Delete Successfully In Gym`,
+            message : `User Delete Successfully In Gym`,
             result : result
         });
 
@@ -95,8 +117,8 @@ const deleteUserInGym = async(req,res)=>{
 const addNewCoachInGym = (req,res) =>{
     const {gym_id, coach_id} = req.body;
     const provider = [gym_id, coach_id];
-    pool.query(`DELETE FROM gym_user WHERE user_id = $2`,provider).then((result) => {
-        pool.query(`INSERT INTO gym_coach (gym_id, user_id) VALUES ($1,$2)`, provider).then((result) => {
+    pool.query(`DELETE FROM gym_user WHERE user_id = $1`,[coach_id]).then((result) => {
+        pool.query(`INSERT INTO gym_coach (gym_id, coach_id) VALUES ($1,$2)`, provider).then((result) => {
             res.status(201).json({
                 success : true,
                 message : `Coach Add Successfully In Gym`,
@@ -120,8 +142,8 @@ const addNewCoachInGym = (req,res) =>{
 
 const getAllCoachInGym = (req,res)=>{
     const gym_id = 2;
-    const provider = [gym_id]
-    pool.query(`SELECT * FROM gym_coach INNER JOIN users ON gym_coach.coach_id = users.id WHERE gym_id = $1`, provider).then((result) => {
+    const provider = [gym_id];
+    pool.query(`SELECT * FROM gym_coach INNER JOIN users ON gym_coach.coach_id = users.id WHERE gym_coach.gym_id = $1`, provider).then((result) => {
         res.status(200).json({
             success : true,
             message : `All Coach In Gym`,
