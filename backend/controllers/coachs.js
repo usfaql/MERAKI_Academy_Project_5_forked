ed85e4dbc6468339a56f210b0d496a1909878b06
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-02-11 18:01:55 +0300

    SOme Edditinng For Add user To Private & remove User From Private Fun

--- backend/controllers/coachs.js
+++ backend/controllers/coachs.js
@@ -1,11 +1,13 @@
 const pool = require("../models/db");
 
-const cteateNewPlane =(req,res)=>{
-    const {name,description,numOfMonth}=req.body
-    const coach_id=req.token.userId
-    const value=[name,description,numOfMonth,coach_id]
-    const query=`INSERT INTO coach_plan (name,description,numOfMonth,coach_id) VALUES ($1,$2,$3,$4) RETURNING *;`
-    pool.query(query,value).then((result)=>{
+const cteateNewPlane = (req, res) => {
+  const { name, description, numOfMonth } = req.body;
+  const coach_id = req.token.userId;
+  const value = [name, description, numOfMonth, coach_id];
+  const query = `INSERT INTO coach_plan (name,description,numOfMonth,coach_id) VALUES ($1,$2,$3,$4) RETURNING *;`;
+  pool
+    .query(query, value)
+    .then((result) => {
       res.status(201).json({
         success: true,
         message: "Plane created successfully",
@@ -18,14 +20,19 @@ const cteateNewPlane =(req,res)=>{
         message: "Server error",
       });
     });
-    
-}
-const AddUserToPrivate=(req,res)=>{
-    const {plan_id,coach_id,private_room_id}=req.body
-    const user_id=req.token.userId
-    const value=[plan_id,coach_id,private_room_id,user_id]
-    const query=`INSERT INTO room_user (plan_id,coach_id,private_room_id,user_id) VALUES ($1,$2,$3,$4) RETURNING *;`
-    pool.query(query,value).then((result)=>{
+};
+const AddUserToPrivate = (req, res) => {
+  const { plan_id, coach_id, private_room_id,numOfMonth} = req.body;
+  const endSub = `CURRENT_TIMESTAMP + INTERVAL '${numOfMonth} months'`;
+  const user_id = 3;
+  const value = [plan_id, coach_id, private_room_id, user_id];
+  
+  const query = `INSERT INTO room_user (plan_id, coach_id, private_room_id, user_id, endSub) 
+  VALUES ($1, $2, $3, $4,${endSub}) 
+  RETURNING *;`;
+  pool
+    .query(query, value)
+    .then((result) => {
       res.status(201).json({
         success: true,
         message: "User Add successfully",
@@ -36,29 +43,32 @@ const AddUserToPrivate=(req,res)=>{
       res.status(400).json({
         success: false,
         message: "Server error",
+        err
       });
     });
-}
-const removeUserFromPrivate=(req,res)=>{
-    const user_id=req.body;
-    const value=[user_id];
-    const query=`UPDATE room_user SET is_delete=1 WHERE user_id=$1`
-    pool.query(query,value).then((result)=>{
-        res.status(201).json({
-          success: true,
-          message: "User Deleted successfully",
-          user: result.rows,
-        });
-      })
-      .catch((err) => {
-        res.status(400).json({
-          success: false,
-          message: "Server error",
-        });
+};
+const removeUserFromPrivate = (req, res) => {
+  const {user_id,coach_id} = req.body;
+  const value = [user_id,coach_id];
+  const query = `UPDATE room_user SET is_delete=1 WHERE user_id=$1 AND coach_id=$2`;
+  pool
+    .query(query, value)
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: "User Deleted successfully",
+        user: result.rows,
       });
-}
-module.exports={
-    cteateNewPlane,
-    AddUserToPrivate,
-removeUserFromPrivate
-}
+    })
+    .catch((err) => {
+      res.status(400).json({
+        success: false,
+        message: "Server error",
+      });
+    });
+};
+module.exports = {
+  cteateNewPlane,
+  AddUserToPrivate,
+  removeUserFromPrivate,
+};
