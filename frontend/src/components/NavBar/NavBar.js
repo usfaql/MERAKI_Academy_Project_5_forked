ac9271ffd3fbf241a6c08fb7bdb2c41c9b2964ed
Yousef Abuaqel <146733467+usfaql@users.noreply.github.com>
2024-02-17 10:48:07 +0300

    Update NavBar.js

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -4,14 +4,14 @@ function NavBar() {
   return (
     <div className='nav-bar'>
         <div>
-            <h2 ><a href='/' style={{textDecoration:"none", color:"white"}}>NUTRI <a style={{color:"#A1E533"}}>FIT</a></a></h2>
+            <h2><a href='/' style={{textDecoration:"none", color:"white"}}>NUTRI <a style={{color:"#A1E533"}}>FIT</a></a></h2>
         </div>
         <div>
-            <lu >
-                <lo className='nav-map'>Home</lo>
-                <lo className='nav-map'>Service</lo>
-                <lo className='nav-map'>About</lo>
-                <lo className='nav-map'>Contact</lo>
+            <lu className="navbar-lu">
+                <li className='nav-map'>Home</li>
+                <li className='nav-map'>Service</li>
+                <li className='nav-map'>About</li>
+                <li className='nav-map'>Contact</li>
             </lu>
         </div>
 
