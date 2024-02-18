ce8bee3c40a338c0d3c7fa124fd58f4c3af463c3
usfaql <usfaql@gmail.com>
2024-02-18 15:08:51 +0300

    Fix Desgin AllGym In Home And Build Gyms Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import {Route, Routes} from "react-router-dom"
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
-import Login from "./components/LoginPage/Login";
+import Login from "./components/Login/Login";
 import NavBar from "./components/NavBar/NavBar";
+import Register from "./components/RegisterPage/Register"
 import NotFound from "./components/NotFound/NotFound";
+import Home from "./components/Home/Home";
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+     <NavBar/>
       <Routes>
+    
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="*" element={<NotFound/>}/>
+        <Route path="/home" element={<Home/>}/>
       </Routes>
     </div>
   );
