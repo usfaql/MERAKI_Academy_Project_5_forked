43bb20d0a9239c2842a46a36750afffe464cd92e
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-02-18 21:36:14 +0300

    Add CoachPrivateSlice

--- frontend/src/components/Redux/Reducers/Store.js
+++ frontend/src/components/Redux/Reducers/Store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Auth/index"
+import coachPrivateReducer from "./CoachPrivate/index"
 export default configureStore({
   reducer: {
     auth:authReducer,
+    coachPrivate:coachPrivateReducer,
   },
 });
