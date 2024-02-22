5400e693628c14ea7d404534a588cfd93d7a9ef3
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-02-23 01:40:46 +0300

    Some Changes

--- frontend/src/components/Home/CoachPrivate/CoachPrivate.js
+++ frontend/src/components/Home/CoachPrivate/CoachPrivate.js
@@ -118,6 +118,14 @@ const CoachPrivate = () => {
     },
   ];
   return (
+    <>    <div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#7aad28" class="bi bi-arrow-left" viewBox="0 0 16 16" className="show" onClick={()=>{
+      setshow(!show)
+    }}>
+      {show?<path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>:<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>}
+
+</svg>
+    </div>
     <div className="Coach-Private-Page">
       {show&&<div className="Left-Side">
         <div className="User-Filter">
@@ -222,15 +230,6 @@ const CoachPrivate = () => {
           </svg>
         </div>
       </div>}
-      
-      <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16" className="show" onClick={()=>{
-        setshow(!show)
-      }}>
-        {show?<path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>:<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>}
-  
-</svg>
-      </div>
       <div className="Right-Side" style={show?{width:"75%"}:{width:"100%"}}>
         <div className="Header">{header}</div>
         <div ref={revarse} className="message">
@@ -279,7 +278,8 @@ const CoachPrivate = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div></>
+    
   );
 };
 
